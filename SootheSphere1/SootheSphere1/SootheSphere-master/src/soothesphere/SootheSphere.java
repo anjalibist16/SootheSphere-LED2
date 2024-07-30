0a56ce66696105201bd326ee7d2e9a3e5a6fba7b
Apala Lamichhane <145183257+apalalamichhane@users.noreply.github.com>
2024-07-30 14:35:58 +0545

    Second Commit

---
+++ SootheSphere1/SootheSphere1/SootheSphere-master/src/soothesphere/SootheSphere.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soothesphere;
+import soothesphere.View.HomePage1;
+import soothesphere.View.LogInPage;
+
+
+
+/**
+ *
+ * @author User
+ */
+public class SootheSphere {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HomePage1 Hp = new HomePage1();
+        Hp.setVisible(true);
+//        LogInPage Lp = new LogInPage();
+//        Lp.setVisible(true);
+         
+
+
+        
+    }
+   
+}
